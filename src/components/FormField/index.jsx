847890e0f550d173027c20f3b0d9703b4bb368ea
Felipe Polato <fepolato@hotmail.com>
2021-09-01 14:04:20 -0300

    formfield com inputs controlados

--- src/components/FormField/index.jsx
+++ src/components/FormField/index.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function FormField() {
+  const [name, setName] = useState("");
+  const [search, setSearch] = useState("");
+  const[date, setDate] = useState("");
+
+  console.log(name);
+  console.log(search);
+  console.log(date)
+
+  const hendlerName = (event) => {
+    setName(event.target.value);
+  };
+
+  const hendlerSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const hendlerDate = (event) => {
+    setDate(event.target.value);
+  };
 
 
-
-
-    
   return (
     <div>
-      
+      <div>
+        <input
+          value={name}
+          type="text"
+          placeholder="Nome"
+          onChange={hendlerName}
+        />
+
+        <label htmlFor="">Data da Entrega</label>
+        <input type="date" onChange={hendlerDate} value={date}/>
+        <button>Lista de Compras</button>
+      </div>
+
+      <button type="submit">Buscar</button>
+      <input
+        type="text"
+        placeholder="Bucar Produto"
+        onChange={hendlerSearch}
+        value={search}
+      />
     </div>
   );
 }
