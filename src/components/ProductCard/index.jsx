4c7f1b3aa5d82c83faa6f727a443edde210bc1c9
Felipe Polato <fepolato@hotmail.com>
2021-09-02 10:18:09 -0300

    mock produtos no componente productcard

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default  function ProductCard(){
+export default function ProductCard() {
+  const [list, setList] = useState([
+    {
+      id: 1,
+      name: "AZEITE",
+      price: 20.49,
+      qty_stock: 158,
+    },
 
-    return(
-        <div>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-            ProductCard
-            <br/>
-        </div>
-    )
+    {
+      id: 2,
+      name: "Shampoo",
+      price: 10.0,
+      qty_stock: 58,
+    },
+  ]);
+
+  return (
+    <div>
+      {list &&
+        list.map((item) => {
+          return (
+            <div>
+              <div>
+                <img src={item.id} />
+                <span>{item.name}</span>
+
+                <button>-</button>
+                <button>+</button>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
