5d17b1aa755ad07618da877511e6c30e16db92d3
Felipe Polato <fepolato@hotmail.com>
2021-09-04 15:07:47 -0300

    estilizacao product card com styled

--- src/screen/Home/style.js
+++ src/screen/Home/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
-    background-color: rgba(142, 177, 211, 0.8);
+    background: #f2f2f2;
     width: 100vw;
     height: 100vh;
 `
